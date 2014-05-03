cf6c2ad196d177a76d9b9def028ab34509a2da3a
avyfain <avyfain@gmail.com>
2014-05-03 17:18:38 -0500

    this is a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BodyAndMind::Application.config.secret_key_base = '30955650ab4bfa29b9eb9e905eb9ffad54fa91db548b3c24de13cf2def12f665e32813ad407dd4d27184fe265510d9be3ef4f8e8174d09ff5a36f358b472750c'
