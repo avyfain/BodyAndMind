da08bc088dac4873e51efe02a745aad778dbe9f8
jcc333 <clemer19@gmail.com>
2014-05-10 17:39:49 -0500

    Seed the Report database with a year of plausible data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+365.times do |n|
+    @date = Time.now - n.day
+    @r = lambda{rand(1..10)}
+    @moods = ["Afraid", "Tense", "Depressed", "Disappointed", "Proud", "Cheerful", "Happy", "Loving", "Humiliated", "Sad"]
+    @report = Report.create!(
+      sleep: @r[],
+      wake: @r[],
+      excercise: @r[],
+      energy: @r[],
+      mood:@moods.sample,
+      weight:160+@r[],
+      stress:@r[],
+      weather: nil,
+      temperature: nil,
+      geolocation: nil
+    )
+    @report.update_attribute :created_at, @date
+    @report.update_attribute :updated_at, @date
+end
+
+
