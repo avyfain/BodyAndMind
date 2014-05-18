1eafb0b33986ac95cb378015a7f6dd091707c15f
Jhanani <jhananisundar@gmail.com>
2014-05-17 21:18:34 -0500

    Added Export Feature for csv and excel files

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,21 @@
 class Report < ActiveRecord::Base
 	attr_accessible :sleep, :wake, :excercise, :energy, :mood, :weight, :stress, :weather, :temperature, :geolocation
-end
 
+  def to_hash
+    sleep_seconds = self.wake - self.sleep
+    @h = self.attributes
+    @h.delete("wake")
+    @h["sleep"] = sleep_seconds/3600
+    return @h
+  end
+
+  def to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << ["created_at","sleep","exercise","energy","mood","weight","stress"]
+      all.each do |report|
+        csv << [report.created_at, report.sleep, report.excercise, report.energy,report.mood,report.weight,report.stress]
+      end
+    end
+  end
+
+end
