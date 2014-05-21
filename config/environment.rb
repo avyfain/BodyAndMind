b709e8d0332cab6c4652689bc5d9775b72c4b1bf
Jhanani <jhananisundar@gmail.com>
2014-05-20 21:42:22 -0500

    Set routes for export

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 BodyAndMind::Application.initialize!
+
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.perform_deliveries = true
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'collaborateur.app@gmail.com',
+    password:             'green394',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ BodyAndMind::Application.routes.draw do
   
   match '/new_report', to: 'reports#new', via: 'get'
   match '/diary', to: 'reports#index', via: 'get'
-  
+
+  match '/views/reports/export.html.erb', to:'reports#export.html.erb',via:'get'
+  match '/views/reports/export.html.erb', to:'reports#show',via:'post'
+
+
 end
