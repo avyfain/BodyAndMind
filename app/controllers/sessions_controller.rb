47cdb84ef543580801d48569e380798e61878368
jcc333 <clemer19@gmail.com>
2014-05-27 18:51:44 -0500

    Users Wip again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,19 @@ class SessionsController < ApplicationController
   def login_attempt
     authorized_user = User.authenticate(params[:email],params[:password])
     if authorized_user
-      flash[:notice] = "Wow Welcome again, you logged in as #{authorized_user.username}"
-      redirect_to(:action => 'index')
+      session[:user_id] = authorized_user.id
+      flash[:notice] = "Welcome, #{authorized_user.username}."
+      redirect_to(:action => 'reports_route')
     else
       flash[:notice] = "Invalid Username or Password"
       flash[:color]= "invalid"
       render "login"
     end
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to :action => 'login'
   end
 
   def home
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,16 @@ BodyAndMind::Application.routes.draw do
   resources :users
   resources :sessions
   
-  match '/signup', to: 'users#new', via: 'get'
-  match '/login', to: 'sessions#login', via: 'get'
   match '/new_report', to: 'reports#new', via: 'get'
   match '/diary', to: 'reports#index', via: 'get'
 
   match '/views/reports/export.html.erb', to:'reports#export.html.erb',via:'get'
   match '/views/reports/export.html.erb', to:'reports#show',via:'post'
+
+
+  match "signup", :to => "users#new", via: 'get'
+  match "login", :to => "sessions#login", via: 'get'
+  match "logout", :to => "sessions#logout", via: 'get'
 
 
 end
